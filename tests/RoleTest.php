4c63b86a6288020406f05e5877d9a6136c7650ec
Valentin PRUGNAUD <valentin@sccnorthwest.com>
2014-06-03 17:29:05 -0700

    Start testing

---
+++ tests/RoleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class RoleTest extends TestCase
+{
+    /** @test */
+    public function it_can_create_a_role()
+    {
+        $role = new Role();
+        $role->name = "Test";
+
+        // @TODO Finish writing test
+    }
+
+}
